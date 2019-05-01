6f50b15fbcc3e02d91764ea00695ba0e430073bd
Charlie Faucheux <charlie-faucheux@hotmail.fr>
2019-05-01 20:28:59 +0200

    Add Ribbon to control navigation between activities

---
+++ Autobox.Desktop/Activities/ActivityManager.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Autobox.Desktop.Activities
+{
+    /// <summary>
+    /// Interaction logic for ActivityManager.xaml
+    /// </summary>
+    public partial class ActivityManager : UserControl
+    {
+        public ActivityManager()
+        {
+            InitializeComponent();
+            Activities = new Dictionary<Button, UserControl>
+            {
+                { PlayerButton, new PlayerActivity() },
+                { LibraryButton, new LibraryActivity() }
+            };
+
+            RibbonButton_Click(PlayerButton, null);
+        }
+
+        private void RibbonButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (CurrentActivityButton != null)
+            {
+                CurrentActivityButton.Background = FindResource("TextSecondaryColor") as Brush;
+                LayoutGrid.Children.Remove(CurrentActivity);
+            }
+
+            CurrentActivityButton = button;
+            CurrentActivityButton.Background = FindResource("TextDarkColor") as Brush;
+            CurrentActivity = Activities[CurrentActivityButton];
+            LayoutGrid.Children.Add(CurrentActivity);
+            Grid.SetRow(CurrentActivity, 1);
+        }
+
+        // ##### Attributes
+        private Button CurrentActivityButton = null;
+        private UserControl CurrentActivity = null;
+        private Dictionary<Button, UserControl> Activities;
+    }
+}
