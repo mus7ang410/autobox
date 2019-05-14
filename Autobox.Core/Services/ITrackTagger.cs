f20ec89347006d2c227f964d9b9539a031e6d1ab
Charlie Faucheux <charlie-faucheux@hotmail.fr>
2019-05-14 19:33:55 +0200

    Add TrackTagger interface

---
+++ Autobox.Core/Services/ITrackTagger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autobox.Core.Data;
+
+namespace Autobox.Core.Services
+{
+    public interface ITrackTagger
+    {
+        Task TagTrackAsync(TrackMetadata track);
+    }
+}
